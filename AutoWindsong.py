49eefc3135f78dc7d7db1f8c6a3d5cdbad5ca1fe
刘宇哲 <yuzhe.liu@mihoyo.com>
2021-03-29 21:02:52 +0800

    上传一版，改线程复用

--- AutoWindsong.py
+++ AutoWindsong.py
@@ -32,40 +32,76 @@ class AutoWindsong(object):
         self.score = score
         self.step_time = 60 / music_speed
         self.speed = 1
+        self.pending_key_info_list = []
+        self._playing_key_info_list = []
+        self._playing_index = 0
+        self._existed_track_thread_list = []
+        self._finished_thread_count = 0
 
     def play(self, speed=1):
         self.speed = speed
+        print('演奏即将开始，请将点击原神并保持在原神界面！')
         for i in range(5, 0, -1):
-            print('启动倒计时：%s秒' % (i))
+            print('启动倒计时：%s秒...' % (i))
             time.sleep(1)
-        for note_info_list in self.score:
-            self._play_note_in_same_pos(note_info_list)
+        self._init_pending_thread_list()
+        self._play()
 
-    def _play_note_in_same_pos(self, note_info_list):
-        thread_list = []
-        for track_id, note_info in enumerate(note_info_list):
-            t = threading.Thread(target=self._play_note, args=(note_info, track_id,))
-            t.start()
-            thread_list.append(t)
-        for t in thread_list:
-            t.join()
+    def _play(self):
+        for idx, key_info_list in enumerate(self.pending_key_info_list):
+            self._playing_index = idx
+            self._playing_key_info_list = key_info_list
+            self._wait_for_all_threads_finish()
+        print('演奏完毕！')
 
-    def _play_note(self, note_info, track_id):
+    def _init_pending_thread_list(self):
+        for note_info_node in self.score:
+            key_info_list = []
+            if isinstance(note_info_node, list):
+                for track_id, note_info_string in enumerate(note_info_node):
+                    key_info = self._get_pending_play_note_info(note_info_string)
+                    key_info_list.append(key_info)
+                    self._get_more_track_thread(track_id)
+            else:
+                note_info_string = note_info_node
+                key_info = self._get_pending_play_note_info(note_info_string)
+                key_info_list.append(key_info)
+                self._get_more_track_thread(0)
+            self.pending_key_info_list.append(key_info_list)
+
+    def _get_more_track_thread(self, track_id):
+        if len(self._existed_track_thread_list) == track_id:
+            self._create_track_thread(track_id)
+
+    def _wait_for_all_threads_finish(self):
+        while self._finished_thread_count != len(self._playing_key_info_list):
+            time.sleep(0.001)
+        self._finished_thread_count = 0
+
+    def _get_pending_play_note_info(self, note_info_string):
+        note_info = note_info_string.split()
         note = note_info[0]
         if len(note_info) >= 2:
-            during_time = note_info[1] / 4 * self.step_time / self.speed
+            during_time = int(note_info[1]) / 4 * self.step_time / self.speed
         else:
             during_time = 1 / 4 * self.step_time / self.speed
-        if len(note_info) >= 3:
-            need_hold = note_info[2]
-        else:
-            need_hold = 0
-        key = note_to_key_dict.get(note)
-        print('track_id:', track_id, note, key, during_time, need_hold)
-        if need_hold:
-            pyautogui.keyDown(key)
-            time.sleep(during_time)
-            pyautogui.keyUp(key)
-        else:
-            pyautogui.press(key)
-            time.sleep(during_time)
+        return [note, during_time]
+
+    def _create_track_thread(self, track_id):
+        t = threading.Thread(target=self._play_note, args=(track_id,))
+        t.start()
+        self._existed_track_thread_list.append(t)
+
+    def _play_note(self, track_id):
+        last_played_index = -1
+        while True:
+            time.sleep(0.001)
+            if last_played_index != self._playing_index and track_id < len(self._playing_key_info_list):
+                last_played_index = self._playing_index
+                note = self._playing_key_info_list[track_id][0]
+                key = note_to_key_dict.get(note)
+                during_time = self._playing_key_info_list[track_id][1]
+                print('track_id:', track_id, note, key, during_time)
+                pyautogui.press(key)
+                time.sleep(during_time)
+                self._finished_thread_count += 1
